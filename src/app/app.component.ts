99d3e89ff77a690bc324ea86d9ade53183baecf9
jaroslaw.kosterkiewicz <jaroslaw.kosterkiewicz@sofomo.com>
2017-12-28 18:42:49 +0100

    Add navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  activeItem = 1;
+
+  onMenuItemClick(value: number) {
+    this.activeItem = value;
+  }
 }
