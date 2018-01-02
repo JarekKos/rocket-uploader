98c7d0281d72f50ac113d54521b2bf056013081f
jaroslaw.kosterkiewicz <jaroslaw.kosterkiewicz@sofomo.com>
2018-01-02 21:16:24 +0100

    Caching images - Delete screen

--- src/app/delete-screen/delete-screen.component.ts
+++ src/app/delete-screen/delete-screen.component.ts
@@ -6,6 +6,7 @@ import { CACHE_KEY_ACTIVE_IMG, CACHE_KEY_DELETE_IMG, SERVER_ADDRESS } from '../c
 import { ImageInterface } from '../interfaces/image-interface';
 import { GetImagesResponseInterface } from '../interfaces/get-images-response-interface';
 import { ChangeImageResponseInterface } from '../interfaces/change-image-response-interface';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-delete-screen',
@@ -20,6 +21,7 @@ export class DeleteScreenComponent implements OnInit {
   constructor(
     private imageLoaderService: ImageLoaderService,
     private cacheService: CacheService,
+    private sanitizer: DomSanitizer,
     @Inject('Window') private window: Window
   ) { }
 
@@ -29,6 +31,15 @@ export class DeleteScreenComponent implements OnInit {
       this.imageLoaderService.getImages(false)
     ).subscribe((serverData: GetImagesResponseInterface) => {
       this.images = serverData.data.uploaded_images;
+      this.images.forEach((image: ImageInterface) => {
+        this.cacheService.get(
+          image.id + '-thumbnail',
+          this.imageLoaderService.getImageThumbnail(image.id, image.original_name)
+        ).subscribe(data => {
+          const file = new File([data], image.original_name);
+          image.url = this.sanitizer.bypassSecurityTrustResourceUrl(window.URL.createObjectURL(file));
+        });
+      });
     });
   }
 
